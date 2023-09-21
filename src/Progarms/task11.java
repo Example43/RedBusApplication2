d5716375ba5a159df9a0ecd55ec4e77bb7fdc29c
Example43 <xyz45038@gmail.com>
2023-09-21 15:25:07 +0530

    git folder

---
+++ src/Progarms/task11.java
@@ -0,0 +1,35 @@
+package Progarms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class task11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      String s = "abcadcccda";
+	      int start = 0;
+	      int end = 0;
+	      int max_length = 0;
+	      List<Character> list =new ArrayList<Character>();
+	      
+	      while(end<s.length()) 
+	      {
+	    	  if(!list.contains(s.charAt(end)))
+	    	  {
+	    		  list.add(s.charAt(end));
+	    		  end++;
+	    		  max_length=Math.max(max_length, list.size());
+	    	  }
+	    	  else
+	    	  {
+	    		  list.remove(Character.valueOf(s.charAt(start)));
+	    		  start++;
+	    	  }    	  
+	      }
+	      System.out.println("maximum length is " +max_length);
+		  }
+
+	}
+
+
---
+++ src/Progarms/task44.java
@@ -0,0 +1,29 @@
+package Progarms;
+
+import java.util.Arrays;
+
+public class task44 {
+	    public static String longestCommonPrefix(String[] strs) {
+		Arrays.sort(strs);
+		  String s1 = strs[0];
+          String s2 = strs[strs.length-1];
+		  int idx = 0;
+		  while(idx < s1.length() && idx < s2.length()){
+		       if(s1.charAt(idx) == s2.charAt(idx)){
+		         idx++;
+		       } else {
+		                break;
+		            }
+		       }
+		        return s1.substring(0, idx);
+		    }
+	    
+        public static void main(String[] args) {
+	        // TODO Auto-generated method stub
+        	
+                System.out.println(longestCommonPrefix(new String[]{"lower", "loght", "loght"}));
+        }
+}
+
+
+
